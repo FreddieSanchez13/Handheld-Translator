bd342619e9f439062c3df9474984ed954b24dd14
Freddie Sanchez <sanchezgoveaf@etown.edu>
2022-12-16 19:43:10 -0500

    no longer needed

--- translateToEnglish.py
+++
@@ -1,6 +0,0 @@
-from googletrans import Translator
-
-def translate_to_english(text: str, source_language: str) -> str:
-    translator = Translator()
-    translation = translator.translate(text, src=source_language, dest='en')
-    return translation.text
